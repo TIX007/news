920258dc6f18b28085bdda76376d4ffa99bc4c96
李崇 <qazmlp2426@qq.com>
2023-02-09 17:49:17 +0800

    中文组件及用户列表

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import IndexRouter from './router/IndexRouter';
+import { ConfigProvider } from 'antd';
+import zh_CN from 'antd/lib/locale-provider/zh_CN';
+import 'moment/locale/zh-cn';
 
 function App() {
-  return <IndexRouter/>
-
+  return <ConfigProvider locale={zh_CN}>
+    <IndexRouter />
+  </ConfigProvider >
 }
 
 export default App;
--- src/views/sandBox/right-manage/RoleList.js
+++ src/views/sandBox/right-manage/RoleList.js
@@ -1,9 +1,80 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+import { Table, Button, Modal, Tree } from 'antd'
+import { DeleteOutlined, EditOutlined, ExclamationCircleOutlined } from '@ant-design/icons'
+const { confirm } = Modal
 
 export default function RoleList() {
+    const [dataSource, setdataSource] = useState([])
+
+    useEffect(() => {
+        axios.get("http://localhost:5000/roles").then(res => {
+            // console.log(res.data)
+            setdataSource(res.data)
+        })
+    }, [])
+
+    // const dataSource = [
+    //     {
+    //         id: '1',
+    //         roleName: '胡彦斌',
+    //         age: 32,
+    //         address: '西湖区湖底公园1号',
+    //     },
+    //     {
+    //         id: '2',
+    //         roleName: '胡彦祖',
+    //         age: 42,
+    //         address: '西湖区湖底公园1号',
+    //     },
+    // ];
+
+    const columns = [
+        {
+            title: 'ID',
+            dataIndex: 'id',
+            render: (id) => {
+                return <b>{id}</b>
+            }
+        },
+        {
+            title: '角色名称',
+            dataIndex: 'roleName'
+        },
+        {
+            title: "操作",
+            render: (item) => {
+                return <div>
+                    <Button danger shape="circle" icon={<DeleteOutlined />} onClick={() => confirmMethod(item)} />
+                    {/* <Button type="primary" shape="circle" icon={<EditOutlined />} onClick={() => {
+                        setisModalVisible(true)
+                        setcurrentRights(item.rights)
+                        setcurrentId(item.id)
+                    }} /> */}
+                </div>
+            }
+        }
+    ]
+
+    const confirmMethod = (item) => {
+        confirm({
+            title: '你确定要删除?',
+            icon: <ExclamationCircleOutlined />,
+            onOk() {
+                console.log('OK');
+                // deleteMethod(item)
+            },
+            onCancel() {
+                console.log('Cancel');
+            },
+        });
+
+    }
+
     return (
         <div>
-            RoleList
+            <Table dataSource={dataSource} columns={columns}
+                rowKey={(item) => item.id} ></Table>
         </div>
     )
 }
